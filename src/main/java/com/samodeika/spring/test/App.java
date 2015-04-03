0932988679c2868c4fd5f88f518ef67c03c19bfe
Alexander Nikolov <alexander.nikolov.alexn@gmail.com>
2015-04-03 17:25:23 +0300

    first commit

---
+++ src/main/java/com/samodeika/spring/test/App.java
@@ -0,0 +1,23 @@
+package com.samodeika.spring.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	
+	public static void main(String[] args){
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/samodeika/spring/test/beans/beans.xml");
+		
+		Person person = (Person)context.getBean("person");
+		person.speak();
+		
+		Address address = (Address)context.getBean("address");
+		
+		System.out.println(person);
+		System.out.println(address);
+		
+		((ClassPathXmlApplicationContext)context).close();
+	}
+
+}
