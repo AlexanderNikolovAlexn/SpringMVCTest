edc6a6b554ace0014d89e8ce9375651a00fcf07d
Alexander Nikolov <alexander.nikolov.alexn@gmail.com>
2015-04-07 12:19:28 +0300

    Adding Authowiring annotation

--- src/main/java/com/samodeika/spring/test/Logger.java
+++ src/main/java/com/samodeika/spring/test/Logger.java
@@ -1,14 +1,18 @@
 package com.samodeika.spring.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Logger {
 
 	private ConsoleWriter consoleWriter;
 	private FileWriter fileWriter;
-
+	
+	@Autowired
 	public void setConsoleWriter(ConsoleWriter consoleWriter) {
 		this.consoleWriter = consoleWriter;
 	}
 
+	@Autowired
 	public void setFileWriter(FileWriter fileWriter) {
 		this.fileWriter = fileWriter;
 	}
