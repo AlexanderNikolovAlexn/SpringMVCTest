b59cc5945869540b25f991e96bd9ee19c84c8acf
Alexander Nikolov <alexander.nikolov.alexn@gmail.com>
2015-04-07 15:18:37 +0300

    SPEL useful operations

--- src/main/java/com/samodeika/spring/test/RandomText.java
+++ src/main/java/com/samodeika/spring/test/RandomText.java
@@ -2,17 +2,21 @@ package com.samodeika.spring.test;
 
 import java.util.Random;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class RandomText {
 
 	private static String[] texts = {
 		"I`ll be back",
 		"Get out",
-		"I want your car"
+		"I want your car",
+		null
 	};
 	
 	public String getText(){
 		Random random = new Random();
-		
+				
 		return texts[random.nextInt(texts.length)];
 	}
 }
--- src/main/java/com/samodeika/spring/test/Robot.java
+++ src/main/java/com/samodeika/spring/test/Robot.java
@@ -1,22 +1,27 @@
 package com.samodeika.spring.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Robot {
 	
-	private int id = 0;
+	private String id = "Default robot";
 	private String speech = "hello";
 	
 	public void speak(){
 		System.out.println(id + ": " + speech);
 	}
 
-	public void setId(int id) {
+	@Autowired
+	public void setId(@Value("#{randomText.text?.length()}") String id) {
 		this.id = id;
 	}
 
-	public void setSpeech(String speech) {
+	@Autowired
+	// new java.util.Date().toString(); T(Math).PI
+	public void setSpeech(@Value("#{T(Math).PI}") String speech) {
 		this.speech = speech;
 	}
 	
